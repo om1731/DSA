d11e0bf69e601268c75a4c6f169762fd604ce148
om1731 <som508822@gmail.com>
2026-04-11 05:53:14 -0700

    Time: 1 ms (100%), Space: 47.3 MB (69.03%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -3,34 +3,13 @@ class Solution {
         int n=nums.length;
         int left=0,right=n-1;
         int[] ans =new int[n];
-        int k=0;
+        int k=n-1;
         while(left<=right){
             if(Math.abs(nums[left])>Math.abs(nums[right])){
-                ans[k++]=nums[left]*nums[left];
+                ans[k--]=nums[left]*nums[left];
                 left++;
-            }else{ans[k++]=nums[right]*nums[right];
+            }else{ans[k--]=nums[right]*nums[right];
             right--;}}
 
-            reverse(ans);
-            return ans ;
-
-            }
-            public void reverse(int[] nums){
-                int left=0,right=nums.length-1;
-               while(left<right){
-                
-                        swap(nums,left,right);
-                        left++;
-                        right--;
-                    }
-                }
-                
-            
-            public void swap(int[] nums,int left,int right){
-                int  temp=nums[left];
-                nums[left]=nums[right];
-                nums[right]=temp;
-            }
-            }
-
-     
+           
+            return ans ;}}
