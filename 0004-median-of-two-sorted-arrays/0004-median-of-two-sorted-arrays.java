e022ceb392ac13d3d204b519878c58424f471334
om1731 <som508822@gmail.com>
2026-04-20 06:12:22 -0700

    Time: 2 ms (45.95%), Space: 48.7 MB (87.89%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,39 +1,43 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
        
-        int[] A = nums1, B = nums2;
-        if (A.length > B.length) {
-            int[] temp = A; A = B; B = temp;
-        }
-
-        int m = A.length, n = B.length;
-        int lo = 0, hi = m;
-
-        while (lo <= hi) {
-            int partA = (lo + hi) / 2;
-            int partB = (m + n + 1) / 2 - partA;
-
-            int maxLeftA  = (partA == 0) ? Integer.MIN_VALUE : A[partA - 1];
-            int minRightA = (partA == m) ? Integer.MAX_VALUE : A[partA];
-
-            int maxLeftB  = (partB == 0) ? Integer.MIN_VALUE : B[partB - 1];
-            int minRightB = (partB == n) ? Integer.MAX_VALUE : B[partB];
-
-            if (maxLeftA <= minRightB && maxLeftB <= minRightA) {
-                
-                if ((m + n) % 2 == 1) {
-                    return Math.max(maxLeftA, maxLeftB);
-                } else {
-                    return (Math.max(maxLeftA, maxLeftB) 
-                          + Math.min(minRightA, minRightB)) / 2.0;
-                }
-            } else if (maxLeftA > minRightB) {
-                hi = partA - 1; 
-            } else {
-                lo = partA + 1;  
-            }
-        }
-
-        return 0.0; 
+   int m=nums1.length,n=nums2.length;
+   int i=0,j=0;
+   int k=m+n;
+   int ind2=k/2;
+   int ind1=ind2-1;
+   int count =0;
+   int ind1el=-1,ind2el=-1;
+   while(i<m && j<n){
+    if(nums1[i]<nums2[j]){
+        if(count==ind1)ind1el=nums1[i];
+        if(count==ind2)ind2el=nums1[i];
+        count++;
+        i++;
     }
+    else{
+        if(count==ind1)ind1el=nums2[j];
+        if(count==ind2)ind2el=nums2[j];
+        count++;
+        j++;
+
+    }
+    }
+while(i<m){
+       if(count==ind1)ind1el=nums1[i];
+        if(count==ind2)ind2el=nums1[i];
+        count++;
+        i++;
+
 }
+while(j<n){
+       if(count==ind1)ind1el=nums2[j];
+        if(count==ind2)ind2el=nums2[j];
+        count++;
+        j++;
+}
+if(k%2==1){
+    return ind2el;
+}
+return (double)((double)(ind1el+ind2el)/2.0);
+   }}
