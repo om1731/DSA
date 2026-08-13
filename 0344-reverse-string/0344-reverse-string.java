5384407247d7f7b62dd8554b0ac0293cb58dda94
om1731 <som508822@gmail.com>
2026-08-14 04:37:22 +0530

    Time: 1 ms (23.08%), Space: 47.9 MB (94.73%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void reverseString(char[] s) {
+        StringBuilder reverse=new StringBuilder(new String(s)).reverse();
+        for(int i=0;i<s.length;i++){
+            s[i]=reverse.charAt(i);
+        }
+        
+    }
+}
